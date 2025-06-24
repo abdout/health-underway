c7fbdd46f82b62384efe39b38ab85350294daeb1
abdout <osmanabdout@hotmail.com>
2025-06-24 19:39:27 +0300

    d

--- src/app/(platform)/dashboard/profile/edit/page.tsx
+++
@@ -1,7 +0,0 @@
-import { editRoutes } from '@/components/onboarding/type';
-import { redirect } from 'next/navigation';
-// import React from 'react';
-
-export default function EditPage() {
-  redirect(editRoutes.ATTACHMENT);
-}
--- src/components/twitter/edit/contribute/validation.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const contributeSchema = z.object({
-  id: z.string().optional(),
-  contribute: z.string().min(3, { message: "يرجى كتابة مساهمتك" }),
-});
-
-export type ContributeSchema = z.infer<typeof contributeSchema>; 
